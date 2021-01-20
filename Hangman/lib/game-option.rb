b3e6e6cb94616a3727d15d41d21bb9005909c6b1
qofgxnm7 <qofgxnm7@gmail.com>
2021-01-20 04:27:32 +0000

    11

--- Hangman/lib/game-option.rb
+++ Hangman/lib/game-option.rb
@@ -20,14 +20,26 @@ class Game
     end
 
     def game_start
-        puts "Please input your first character."
-        input_check()
-        print @tip.join(" ")
+        turns = 1
+        for i in 1..10
+            if turns == 1
+                puts "Please input your first character!"
+                input_check()
+                print @tip.join(" ")
+            elsif turns < 11
+                puts "Remaining #{10 - turns} chances to try!"
+                input_check()
+                print @tip.join(" ")
+            else
+                puts "Game Over!"
+            end
+            turns += 1
+            break if !(@tip.include?("_"))
 
+        end
+        puts " Congratulation! You win!" if !(@tip.include?("_"))
     end
-
-
 end
 
-gamee = Game.new
-gamee.game_start
+play = Game.new
+play.game_start
